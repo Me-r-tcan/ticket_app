15e50e85adef10776a6aba101418107ad0d8cfe0
Mertcan bas <mertcanbas35@gmail.com>
2022-06-26 10:11:16 +0300

    add tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -53,3 +53,15 @@ it("Disallows duplicate emails", async () => {
     })
     .expect(400);
 });
+
+it("Sets a cookie after successful signup", async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
