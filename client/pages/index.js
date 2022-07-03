9886bc10271e9ef80dadfeadb8fea122c2d1407a
Mertcan bas <mertcanbas35@gmail.com>
2022-07-03 12:30:49 +0300

    refactor get initial props

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,30 +1,20 @@
 import axios from "axios";
 
-const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
+import buildClient from "../api/build-client";
 
-  return <h1>Landing Page</h1>;
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    // we are on the server
-    // requests should be made to http://ingress-nginx.ingress.nginx.
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: req.headers,
-      }
-    );
-    return data;
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url of ''
-    const { data } = await axios.get("/api/users/currentuser");
-    return data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
 
-  return {};
+  return data;
 };
 
 export default LandingPage;
