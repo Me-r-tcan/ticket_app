3450fd9a91825c274cb20394dd88ee59e73d50e9
Mertcan bas <mertcanbas35@gmail.com>
2022-07-04 13:12:46 +0300

    add new test for tickets

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,11 +1,12 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import request from "supertest";
+import jwt from "jsonwebtoken";
 
 import { app } from "../app";
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
@@ -35,20 +36,25 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = "test@test.com";
-  const password = "password";
+global.signin = () => {
+  // Build a JWT payload. { id, email }
+  const payload = {
+    id: "1lk24j124l",
+    email: "test@test.com",
+  };
 
-  // Bilerek signup rotası kullanıldı
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email,
-      password,
-    })
-    .expect(201);
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  const cookie = response.get("Set-Cookie");
+  // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
 
-  return cookie;
+  // Turn that session into JSON
+  const sessionJson = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJson).toString("base64");
+
+  // return a string thats the cookie with encoded data
+  return [`express:sess=${base64}`];
 };
